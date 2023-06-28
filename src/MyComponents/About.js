112d2109db61ec07b5b6d09adb6f1847d49e43c9
Kush-munot <munot.kgm@gmail.com>
2023-06-28 21:17:45 +0530

    :zap: Project done !!

---
+++ src/MyComponents/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div style={{paddingLeft:'2rem'}}>
+            <br/>
+            <p>Hey, I am Kush Munot. A Front-end Developer and React Developer from India who
+                    loves to design and code. I use React.js or Next.js to build the web
+                    application interfaces and the functionalities. At the moment, I am
+                    pursuing my Bachelor's degree in Computer Science.<br /><br />
+                    The first programming language I learned was C. Since then, I have also
+                    learned Python. I also played around with databases, but Firebase is my
+                    favorite right now.<br />
+                    Aside from coding, I love reading books and cooking as well. I don't have
+                    the time or really any interest in playing video games these days. However,
+                    I also enjoy watching movies and web series whenever I have the chance.<br /><br />
+                    If you were to ask me what stack I would prefer, I would say<br /><br />
+
+                    - Framework - React.js, Typescript, TailwindCSS<br />
+                    - Database - Firebase and MongoDB<br />
+                    - UI Library - Material UI<br /><br />
+
+                    You can do anything you can think of with these three things. I have done
+                    two paid Internships into early stage Startups as Full Stack Developer.
+                    I prefer making projects with my team and friends.<br /><br />
+                    I love my work and enjoy each and every project as I get it. Feel free to look at my portfolio and don't hesitate to contact me.<br /><br /></p>
+        </div>
+    )
+}
